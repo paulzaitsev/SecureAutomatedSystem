cffcb171350f517f495ca012187be3676e9e911b
paulzaitsev <paulzaitsevb@gmail.com>
2016-05-25 10:02:59 +0300

    #product serialization

--- SecureAutomatedSystem/ProcessEmulation/Product.cs
+++ SecureAutomatedSystem/ProcessEmulation/Product.cs
@@ -1,18 +1,45 @@
-﻿namespace SecureAutomatedSystem.ProcessEmulation {
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SecureAutomatedSystem.ProcessEmulation {
+    [Serializable]
     public class Product {
-        public float OuterDiameter { get; private set; }
-        public float OuterRadius { get; private set; }
-        public float InnerDiameter { get; private set; }
-        public float InnerRadius { get; private set; }
-        public float OuterPairingRadius { get; private set; }
-        public float WallThickness { get; private set; }
-        public float TopThickness { get; private set; }
-        public float BottomLowersectionHeight { get; private set; }
-        public float TopDiameter { get; private set; }
-        public float OuterPairingRadiusCyl { get; private set; }
-             
-        public Product(float p1, float p2, float p3, float p4, float p5, float p6, float p7, float p8, float p9,
-            float p10) {
+        [XmlAttributeAttribute("outer_diameter")]
+        public float OuterDiameter { get; set; }
+
+        [XmlAttributeAttribute("outer_radius")]
+        public float OuterRadius { get; set; }
+
+        [XmlAttributeAttribute("inner_diameter")]
+        public float InnerDiameter { get; set; }
+
+        [XmlAttributeAttribute("inner_radius")]
+        public float InnerRadius { get; set; }
+
+        [XmlAttributeAttribute("outer_pairing_radius")]
+        public float OuterPairingRadius { get; set; }
+
+        [XmlAttributeAttribute("wall_thickness")]
+        public float WallThickness { get; set; }
+
+        [XmlAttributeAttribute("top_thickness")]
+        public float TopThickness { get; set; }
+
+        [XmlAttributeAttribute("bottom_lowersection_height")]
+        public float BottomLowersectionHeight { get; set; }
+
+        [XmlAttributeAttribute("top_diameter")]
+        public float TopDiameter { get; set; }
+
+        [XmlAttributeAttribute("outer_pairing_radius_cyl")]
+        public float OuterPairingRadiusCyl { get; set; }
+
+        public Product() { }
+
+        public Product(float p1, float p2, float p3, float p4, float p5, float p6, float p7, float p8, float p9, float p10) {
             OuterDiameter = p1;
             OuterRadius = p2;
             InnerDiameter = p3;
@@ -25,13 +52,43 @@
             OuterPairingRadiusCyl = p10;
         }
 
-        public override string ToString() {
+        public void Assign(Product source) {
+            if (source != null) {
+                this.OuterDiameter = source.OuterDiameter;
+                this.OuterRadius = source.OuterRadius;
+                this.InnerDiameter = source.InnerDiameter;
+                this.InnerRadius = source.InnerRadius;
+                this.OuterPairingRadius = source.OuterPairingRadius;
+                this.WallThickness = source.WallThickness;
+                this.TopThickness = source.TopThickness;
+                this.BottomLowersectionHeight = source.BottomLowersectionHeight;
+                this.TopDiameter = source.TopDiameter;
+                this.OuterPairingRadiusCyl = source.OuterPairingRadiusCyl;
+            }
+        }
+
+
+        public override string ToString() { //?
             return string.Format("OuterDiameter = {0} \nOuterRadius = {1} \nInnerDiameter = {2}" +
-                                "\nInnerRadius = {3} \nOuterPairingRadius = {4} \nWallThickness = {5}" +
-                                "\nTopThickness = {6} \nBottomLowersectionHeight = {7} \nTopDiameter = {8}" +
-                                "\nOuterPairingRadiusCylindrical = {9}", OuterDiameter, OuterRadius, InnerDiameter,
-                                InnerRadius, OuterPairingRadius, WallThickness, TopThickness, BottomLowersectionHeight,
-                                TopDiameter, OuterPairingRadiusCyl);
+                                 "\nInnerRadius = {3} \nOuterPairingRadius = {4} \nWallThickness = {5}" +
+                                 "\nTopThickness = {6} \nBottomLowersectionHeight = {7} \nTopDiameter = {8}" +
+                                 "\nOuterPairingRadiusCylindrical = {9}", OuterDiameter, OuterRadius, InnerDiameter,
+                InnerRadius, OuterPairingRadius, WallThickness, TopThickness, BottomLowersectionHeight,
+                TopDiameter, OuterPairingRadiusCyl);
+        }
+
+        public void DeserializeFromXml(string xmlFileName) {
+            XmlSerializer mySerializer = new XmlSerializer(typeof (Product));
+            using (FileStream myFileStream = new FileStream(xmlFileName, FileMode.Open)) {
+                this.Assign((Product) mySerializer.Deserialize(myFileStream));
+            }
+        }
+
+        public void SerializeToXml(string xmlFileName) {
+            XmlSerializer serializer = new XmlSerializer(typeof (Product));
+            using (FileStream fs = new FileStream(xmlFileName, FileMode.OpenOrCreate)) {
+                serializer.Serialize(fs, this);
+            }
         }
     }
 }
